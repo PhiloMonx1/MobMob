82df44562cd4f38d265023c07cff43b46db7b707
2m <philomonx1@gmail.com>
2022-05-24 19:04:08 +0900

    0.2

--- JS/todos.js
+++ JS/todos.js
@@ -24,10 +24,19 @@ function appendTodos(newTodo){
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("i")
-    button.setAttribute("class", "fa-solid fa-circle-xmark");
-    button.addEventListener("click", deleteTodos);
+    if(localStorage.getItem(li.id) === "true"){ //checkTodos on
+        button.setAttribute("class", "fa-regular fa-square-check");
+    }else{
+        button.setAttribute("class", "fa-regular fa-square");
+        localStorage.setItem(li.id, "false") //checkTodos new&off
+    }
+    button.addEventListener("click", checkTodos);
+    const more = document.createElement("i")
+    more.setAttribute("class", "fa-solid fa-ellipsis");
+    more.addEventListener("click", deleteTodos);
     li.appendChild(button);
     li.appendChild(span);
+    li.appendChild(more);
     todoList.appendChild(li);
 }
 function deleteTodos(btn){
@@ -35,13 +44,27 @@ function deleteTodos(btn){
     toDos = toDos.filter(toDos => toDos.id !== parseInt(me.id))
     me.remove();
     saveToDos();
+    localStorage.removeItem(me.id) //checkTodos
 }
 function saveToDos(){
     localStorage.setItem(TODO_LIST ,JSON.stringify(toDos))
 }
+function checkTodos(check){
+    const parent = check.target.parentElement;
+    if(localStorage.getItem(parent.id) === "false"){
+        check.target.className = "fa-regular fa-square-check";
+        localStorage.setItem(parent.id, "true")
+    }else if(localStorage.getItem(parent.id) === "true"){
+        check.target.className = "fa-regular fa-square";
+        localStorage.setItem(parent.id, "false")
+    }else{
+        alert("miss!!");
+        localStorage.setItem(parent.id, "false")
+    }
+}
 
 todoForm.addEventListener("submit", submitTodos);
- 
+
 if(!(localStorage.getItem(TODO_LIST) == null)){
     toDos = JSON.parse(localStorage.getItem(TODO_LIST));
     toDos.forEach(appendTodos);
